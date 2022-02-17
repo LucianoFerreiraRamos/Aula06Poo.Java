274614d7b24f65dfd34f4917d534040119af09ba
Luciano <97047885+LucianoFerreiraRamos@users.noreply.github.com>
2022-02-17 15:11:04 -0300

    Aula06Poo

---
+++ src/aula06poo/Aula06Poo.java
@@ -0,0 +1,13 @@
+package aula06poo;
+public class Aula06Poo {
+	public static void main(String[] args) {
+		
+		ControleRemoto c = new ControleRemoto();
+		c.ligar();
+		c.maisVolume();
+		c.play();
+		c.abrirMenu();
+		c.fecharMenu();
+	}
+
+}
